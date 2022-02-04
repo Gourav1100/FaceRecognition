2c4baf3bb40629dbaec10fcd3aa05bc40817363c
Gourav1100 <gauravb1100@gmail.com>
2022-02-04 20:30:46 +0530

    updated train.py and added trained models for differnet features

--- train.py
+++ train.py
@@ -6,9 +6,8 @@ import numpy as np
 
 # import keras and tensorflow modules
 from keras.optimizer_v2.adam import Adam
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Dense, MaxPooling2D, Flatten
-import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Conv2D, Dense, MaxPooling2D, Flatten, Dropout
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -26,30 +25,51 @@ from sklearn.metrics import accuracy_score
 class faceTrain:
     # initialise faceTrain Class
     def __init__(self):
-        self.load_target()
+        self.epochs = 5
+        self.load_target_map()
         self.load_feature()
         self.image_x = len(self.feature[0])
         self.image_y = len(self.feature[0][0])
-        self.train_test_Split()
-        self.state = self.architecture()
-        print(self.state)
-        if self.state == False:
-            print("Aborting")
 
-    # load target data fron csv file
-    def load_target(self):
+    # load target data and create a map fron csv file
+    def load_target_map(self):
         self.labels = list()
         print("loading csv data...")
         self.target = pd.read_csv("./dataset/facial_keypoints.csv")
-        target = list()
+        self.target_map = {'nose': [],'eyebrow_left': [],'eyebrow_right': [],'eye_left': [],'eye_right': [],'mouth_left': [],'mouth_right': [],'mouth_top': [],'mouth_bottom': []}
+        mouth_left = ['mouth_left_corner_x', 'mouth_left_corner_y']
+        mouth_top = ['mouth_center_top_lip_x', 'mouth_center_top_lip_y']
+        mouth_bottom = ['mouth_center_bottom_lip_x', 'mouth_center_bottom_lip_y']
+        mouth_right = ['mouth_right_corner_x', 'mouth_right_corner_y']
+        eye_left = ['left_eye_center_x', 'left_eye_center_y','left_eye_inner_corner_x', 'left_eye_inner_corner_y', 'left_eye_outer_corner_x', 'left_eye_outer_corner_y']
+        eye_right = ['right_eye_center_x', 'right_eye_center_y','right_eye_inner_corner_x', 'right_eye_inner_corner_y', 'right_eye_outer_corner_x', 'right_eye_outer_corner_y']
+        eyebrow_left = ['left_eyebrow_inner_end_x', 'left_eyebrow_inner_end_y', 'left_eyebrow_outer_end_x', 'left_eyebrow_outer_end_y']
+        eyebrow_right = ['right_eyebrow_inner_end_x', 'right_eyebrow_inner_end_y', 'right_eyebrow_outer_end_x', 'right_eyebrow_outer_end_y']
+        nose = ['nose_tip_x', 'nose_tip_y']
         for i in self.target.keys():
             temp = []
             for j in range(len(self.target[i])):
                 temp.append(self.target[i][j])
             self.labels.append(i)
-            target.append(temp)
-        self.target = np.array(target)
-        self.target = self.target.transpose((1,0))
+            if i in nose:
+                self.target_map['nose'].append(temp)
+            elif i in eye_left:
+                self.target_map['eye_left'].append(temp)
+            elif i in eye_right:
+                self.target_map['eye_right'].append(temp)
+            elif i in eyebrow_left:
+                self.target_map['eyebrow_left'].append(temp)
+            elif i in eyebrow_right:
+                self.target_map['eyebrow_right'].append(temp)
+            elif i in mouth_left:
+                self.target_map['mouth_left'].append(temp)
+            elif i in mouth_right:
+                self.target_map['mouth_right'].append(temp)
+            elif i in mouth_top:
+                self.target_map['mouth_top'].append(temp)
+            elif i in mouth_bottom:
+                self.target_map['mouth_bottom'].append(temp)
+        print('Loading csv...done')
 
     # load feature data fron jpeg files
     def load_feature(self):
@@ -71,6 +91,12 @@ class faceTrain:
         print("loading...done")
         self.feature = np.array(self.feature)
 
+    # load target from target_map
+    def load_target(self,id):
+        self.target = self.target_map[id]
+        self.target = np.array(self.target)
+        self.target = self.target.transpose((1,0))
+
     # split data for training and testing
     def train_test_Split(self):
         self.train_feature,self.test_feature,self.train_target,self.test_target = train_test_split(self.feature, self.target, test_size = 0.1)
@@ -80,14 +106,16 @@ class faceTrain:
     # define model ,optimisers , lossfn
     def architecture(self):
         try:
+            self.model = True
             self.model = Sequential()
-            self.model.add(Conv2D(64,(7,7),input_shape=(self.image_x,self.image_y,1),activation="relu"))
+            self.model.add(Conv2D(32,(3,3),input_shape=(self.image_x,self.image_y,1),activation="relu"))
+            self.model.add(MaxPooling2D(32,(2,2),padding="same"))
+            self.model.add(Conv2D(64,(3,3),activation="relu"))
             self.model.add(MaxPooling2D(64,(2,2),padding="same"))
-            self.model.add(Conv2D(128,(4,4),activation="relu"))
-            self.model.add(MaxPooling2D(64,(2,2),padding="same"))
-            self.model.add(Conv2D(128,(4,4),activation="relu"))
+            self.model.add(Conv2D(64,(3,3),activation="relu"))
             self.model.add(Flatten())
-            self.model.add(Dense(30,activation="sigmoid"))
+            self.model.add(Dropout(0.3))
+            self.model.add(Dense(len(self.target[0]),activation="sigmoid"))
         except:
             print("Unable to Initialize model")
             return False
@@ -95,30 +123,38 @@ class faceTrain:
 
     # train defined model and fit values
     def train(self):
-        # create augmented images
-        aug_data_gen=ImageDataGenerator(width_shift_range=0,height_shift_range=0,zoom_range=0,shear_range=0,rotation_range=0)
-        aug_data_gen.fit(self.train_feature)
-        if self.state == True:
+        for i in self.target_map.keys():
+            print(f"---------- training model for {i} ----------")
+            self.load_target(i)
+            self.state = self.architecture()
+            if self.state == False:
+                print("Unable to set model architecture! Aborting.")
+                return False
+            self.train_test_Split()
+            # create augmented images
+            aug_data_gen=ImageDataGenerator(width_shift_range=0,height_shift_range=0,zoom_range=0,shear_range=0,rotation_range=0)
+            aug_data_gen.fit(self.train_feature)
             self.model.compile(Adam(learning_rate=0.001),loss="categorical_crossentropy",metrics=["accuracy"])
-            self.model.fit(aug_data_gen.flow(self.train_feature,self.train_target,batch_size=128),epochs=10)
-        else:
-            print("Aborting! Model not initialised.")
+            self.model.fit(aug_data_gen.flow(self.train_feature,self.train_target,batch_size=128),epochs=self.epochs,validation_data=(self.test_feature,self.test_target))
+            if self.save_model(i) == False:
+                print("Unable to save the model to file!!")
+                break
+            print(f"---------- {i} Model trained and saved ----------")
 
     # save defined model
-    def save_model(self):
+    def save_model(self,name):
         # Lets save training data to json file to reduce runtime at next run
-        json_model = self.model.to_json()
-        with open( './json_model/trained_model.json', 'w' ) as json_file:
-            json_file.write(json_model)
-        self.model.save_weights('./json_model/trained_model.h5')
-        print(self.model.summary())
-        print("Saved model to drive")
-        print("saved")
+        try:
+            json_model = self.model.to_json()
+            with open( './json_model/'+name+'.json', 'w' ) as json_file:
+                json_file.write(json_model)
+            self.model.save_weights('./json_model/'+name+'.h5')
+            print(self.model.summary())
+            print(f"Saved {name} model to drive")
+        except:
+            return False
+        return True
 
-    # test model on testing feature and target
-    def test(self):
-        print("testing")
 
 test = faceTrain()
 test.train()
-test.test()
