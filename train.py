0d0aae7646ed0ef1eb3505cdb2c9076ed41a534f
Gourav1100 <gauravb1100@gmail.com>
2022-02-05 21:20:20 +0530

    changed training model and completed faceTrain class.

--- train.py
+++ train.py
@@ -1,160 +1,202 @@
 # Importing all dependencies
 # importing the libraries
-import cv2
-import pandas as pd
+import os
 import numpy as np
-
 # import keras and tensorflow modules
-from keras.optimizer_v2.adam import Adam
-from keras.models import Sequential
-from keras.layers import Conv2D, Dense, MaxPooling2D, Flatten, Dropout
-from sklearn.model_selection import train_test_split
-from keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+# Matplotlib to plot test predictions
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
-# for reading
-from skimage.io import imread
-from skimage.transform import resize
-
-# for creating validation set
-from sklearn.model_selection import train_test_split
-
-# for evaluating the model
-from sklearn.metrics import accuracy_score
 
 # Training Class
 class faceTrain:
     # initialise faceTrain Class
     def __init__(self):
-        self.epochs = 5
-        self.load_target_map()
-        self.load_feature()
-        self.image_x = len(self.feature[0])
-        self.image_y = len(self.feature[0][0])
+        self.epochs = 250
+        self.state = False
 
-    # load target data and create a map fron csv file
-    def load_target_map(self):
-        self.labels = list()
-        print("loading csv data...")
-        self.target = pd.read_csv("./dataset/facial_keypoints.csv")
-        self.target_map = {'nose': [],'eyebrow_left': [],'eyebrow_right': [],'eye_left': [],'eye_right': [],'mouth_left': [],'mouth_right': [],'mouth_top': [],'mouth_bottom': []}
-        mouth_left = ['mouth_left_corner_x', 'mouth_left_corner_y']
-        mouth_top = ['mouth_center_top_lip_x', 'mouth_center_top_lip_y']
-        mouth_bottom = ['mouth_center_bottom_lip_x', 'mouth_center_bottom_lip_y']
-        mouth_right = ['mouth_right_corner_x', 'mouth_right_corner_y']
-        eye_left = ['left_eye_center_x', 'left_eye_center_y','left_eye_inner_corner_x', 'left_eye_inner_corner_y', 'left_eye_outer_corner_x', 'left_eye_outer_corner_y']
-        eye_right = ['right_eye_center_x', 'right_eye_center_y','right_eye_inner_corner_x', 'right_eye_inner_corner_y', 'right_eye_outer_corner_x', 'right_eye_outer_corner_y']
-        eyebrow_left = ['left_eyebrow_inner_end_x', 'left_eyebrow_inner_end_y', 'left_eyebrow_outer_end_x', 'left_eyebrow_outer_end_y']
-        eyebrow_right = ['right_eyebrow_inner_end_x', 'right_eyebrow_inner_end_y', 'right_eyebrow_outer_end_x', 'right_eyebrow_outer_end_y']
-        nose = ['nose_tip_x', 'nose_tip_y']
-        for i in self.target.keys():
-            temp = []
-            for j in range(len(self.target[i])):
-                temp.append(self.target[i][j])
-            self.labels.append(i)
-            if i in nose:
-                self.target_map['nose'].append(temp)
-            elif i in eye_left:
-                self.target_map['eye_left'].append(temp)
-            elif i in eye_right:
-                self.target_map['eye_right'].append(temp)
-            elif i in eyebrow_left:
-                self.target_map['eyebrow_left'].append(temp)
-            elif i in eyebrow_right:
-                self.target_map['eyebrow_right'].append(temp)
-            elif i in mouth_left:
-                self.target_map['mouth_left'].append(temp)
-            elif i in mouth_right:
-                self.target_map['mouth_right'].append(temp)
-            elif i in mouth_top:
-                self.target_map['mouth_top'].append(temp)
-            elif i in mouth_bottom:
-                self.target_map['mouth_bottom'].append(temp)
-        print('Loading csv...done')
+    def test(self):
+        if self.state == False:
+            print("\n\r Model Not Available")
+            return False
+        self.load_feature_n_target()
+        print("\n\rtesting model...")
+        fig = plt.figure(figsize=( 50 , 50 ))
 
-    # load feature data fron jpeg files
-    def load_feature(self):
-        self.feature = list()
-        print("loading image data...")
-        for img_name in range(len(self.target)):
-            image_path = './dataset/jpegs/face_' + str(img_name) + '.jpeg'
-            if img_name%1000 == 0:
-                print("loading: [ "+str(img_name)+" - "+ str(img_name+1000)+" ]")
-            img = imread(image_path)
-            img =cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            img = img/255
-            # resizing the image to (224,224,3)
-            img = resize(img, output_shape=(96,96,1), mode='constant', anti_aliasing=True)
-            # converting the type of pixel to float 32
-            img = img.astype('float32')
-            # appending the image into the list
-            self.feature.append(img)
-        print("loading...done")
-        self.feature = np.array(self.feature)
+        for i in range( 1 , 6 ):
+            sample_image = np.reshape( self.test_feature[i] * 255  , ( 96 , 96 ) ).astype( np.uint8 )
+            pred = self.model.predict( self.test_feature[ i : i +1  ] ) * 96
+            pred = pred.astype( np.int32 )
+            pred = np.reshape( pred[0 , 0 , 0 ] , ( 15 , 2 ) )
+            fig.add_subplot( 1 , 10 , i )
+            plt.imshow( sample_image.T , cmap='gray' )
+            plt.scatter( pred[ : , 0 ] , pred[ : , 1 ] , c='green' )
 
-    # load target from target_map
-    def load_target(self,id):
-        self.target = self.target_map[id]
-        self.target = np.array(self.target)
-        self.target = self.target.transpose((1,0))
+        print("showing predictions now...")
+        plt.show()
+        # show predictions on tesing data
+        print("testing model... done.")
 
-    # split data for training and testing
-    def train_test_Split(self):
-        self.train_feature,self.test_feature,self.train_target,self.test_target = train_test_split(self.feature, self.target, test_size = 0.1)
-        self.train_feature=self.train_feature.reshape(len(self.train_feature),self.image_x,self.image_y,1)
-        self.test_feature=self.test_feature.reshape(len(self.test_feature),self.image_x,self.image_y,1)
+    # predict locations
+    def predict(self,img,model):
+        if self.load_model(model):
+            img = img / 255
+            img = np.reshape( img , ( 96 , 96 ) ).astype(np.uint8)
+            predictions = self.model.predict(img) * 96
+            predictions = predictions.astype( np.int32 )
+            predictions = np.reshape( predictions[ 0 , 0 , 0 ], ( 15 , 2 ) )
+            return predictions
+        else:
+            return False
+
+
+    # autotrain function
+    def auto_train(self):
+                # use GPU if available
+        gpus = tf.config.list_physical_devices('GPU')
+        if gpus:
+        # Restrict TensorFlow to only use the first GPU
+            try:
+                tf.config.set_visible_devices(gpus[0], 'GPU')
+                logical_gpus = tf.config.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+            except RuntimeError as e:
+                # Visible devices must be set before GPUs have been initialized
+                print(e)
+        self.load_feature_n_target()
+        self.train("trained_model")
+
+
+    # load feature data and target data fron jpeg files
+    def load_feature_n_target(self):
+        try:
+            print("\n\r\n\rloading training and testing data...")
+
+            self.train_feature = np.load("./dataset/x_train.npy")
+            self.train_feature = self.train_feature / 255
+            print(self.train_feature.shape)
+            self.train_target = np.load("./dataset/y_train.npy")
+            self.train_target = self.train_target / 96
+            self.train_target = np.reshape( self.train_target, ( -1 , 1 , 1 , 30 ))
+            print(self.train_target.shape)
+            self.test_feature = np.load("./dataset/x_test.npy")
+            self.test_feature = self.test_feature / 255
+            print(self.test_feature.shape)
+            self.test_target = np.load("./dataset/y_test.npy")
+            self.test_target = self.test_target / 96
+            self.test_target =np.reshape( self.test_target, ( -1 , 1 , 1 , 30 ) )
+            print(self.test_target.shape)
+
+            self.image_x = self.train_feature.shape[1]
+            self.image_y = self.train_feature.shape[0]
+            print("loading training and testing data... done!")
+        except:
+            print("\n\r Unable to load data \n\r")
+            self.state = False
+            return False
 
     # define model ,optimisers , lossfn
     def architecture(self):
         try:
-            self.model = True
-            self.model = Sequential()
-            self.model.add(Conv2D(32,(3,3),input_shape=(self.image_x,self.image_y,1),activation="relu"))
-            self.model.add(MaxPooling2D(32,(2,2),padding="same"))
-            self.model.add(Conv2D(64,(3,3),activation="relu"))
-            self.model.add(MaxPooling2D(64,(2,2),padding="same"))
-            self.model.add(Conv2D(64,(3,3),activation="relu"))
-            self.model.add(Flatten())
-            self.model.add(Dropout(0.3))
-            self.model.add(Dense(len(self.target[0]),activation="sigmoid"))
+            model_layers = [
+            tf.keras.layers.Conv2D( 256 , input_shape=( 96 , 96 , 1 ) , kernel_size=( 3 , 3 ) , strides=2 , activation='relu' ),
+            tf.keras.layers.Conv2D( 256 , kernel_size=( 3 , 3 ) , strides=2 , activation='relu' ),
+            tf.keras.layers.BatchNormalization(),
+
+            tf.keras.layers.Conv2D( 128 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+            tf.keras.layers.Conv2D( 128 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+            tf.keras.layers.BatchNormalization(),
+
+            tf.keras.layers.Conv2D( 128 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+            tf.keras.layers.Conv2D( 128 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+            tf.keras.layers.BatchNormalization(),
+
+            tf.keras.layers.Conv2D( 64 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+            tf.keras.layers.Conv2D( 64 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+            tf.keras.layers.BatchNormalization(),
+
+            tf.keras.layers.Conv2D( 32 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+            tf.keras.layers.Conv2D( 32 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+            tf.keras.layers.BatchNormalization(),
+
+            tf.keras.layers.Conv2D( 30 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+            tf.keras.layers.Conv2D( 30 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+            tf.keras.layers.Conv2D( 30 , kernel_size=( 3 , 3 ) , strides=1 ),
+            ]
+            self.model = tf.keras.Sequential( model_layers )
         except:
             print("Unable to Initialize model")
             return False
         return True
 
     # train defined model and fit values
-    def train(self):
-        for i in self.target_map.keys():
-            print(f"---------- training model for {i} ----------")
-            self.load_target(i)
+    def train(self, name):
+        models = os.listdir("./json_model/")
+        if name+".h5" not in models or name+".json" not in models:
             self.state = self.architecture()
             if self.state == False:
                 print("Unable to set model architecture! Aborting.")
                 return False
-            self.train_test_Split()
-            # create augmented images
-            aug_data_gen=ImageDataGenerator(width_shift_range=0,height_shift_range=0,zoom_range=0,shear_range=0,rotation_range=0)
-            aug_data_gen.fit(self.train_feature)
-            self.model.compile(Adam(learning_rate=0.001),loss="categorical_crossentropy",metrics=["accuracy"])
-            self.model.fit(aug_data_gen.flow(self.train_feature,self.train_target,batch_size=128),epochs=self.epochs,validation_data=(self.test_feature,self.test_target))
-            if self.save_model(i) == False:
+            try:
+                self.model.compile( loss=tf.keras.losses.mean_squared_error , optimizer=tf.keras.optimizers.Adam( learning_rate=0.0001 ) , metrics=[ 'mse' ] )
+            except:
+                print("Unable to compile model")
+                return False
+            print("\n\r---------- training model started ----------")
+
+            try:
+                self.model.fit(x=self.train_feature,y=self.train_target,batch_size=50,epochs=self.epochs,validation_data=(self.test_feature,self.test_target))
+            except RuntimeError as err:
+                print("Error encountered while training")
+                print(err)
+                return False
+            if self.save_model(name) == False:
                 print("Unable to save the model to file!!")
-                break
-            print(f"---------- {i} Model trained and saved ----------")
+            else:
+                print("Model saved to disk")
+
+            print("---------- Model training Completed ----------")
+        else:
+            self.load_model(name)
+            return True
+
 
     # save defined model
-    def save_model(self,name):
-        # Lets save training data to json file to reduce runtime at next run
+    def save_model(self, name):
+        if self.state == False:
+            print("Model not available")
+            return False
+        # save training data
         try:
             json_model = self.model.to_json()
             with open( './json_model/'+name+'.json', 'w' ) as json_file:
                 json_file.write(json_model)
             self.model.save_weights('./json_model/'+name+'.h5')
             print(self.model.summary())
-            print(f"Saved {name} model to drive")
+            print(f"Saved {name} to drive.")
         except:
             return False
         return True
 
+    # load model from disk
+    def load_model(self, name):
+        location = ["./json_model/"+name+".json", "./json_model/"+name+".h5"]
+        try:
+            file = open(location[0])
+            self.model = file.read()
+            file.close()
+            self.model = tf.keras.models.model_from_json(self.model)
+            self.model.load_weights(location[1])
+            self.state = 1
+            print(self.model.summary())
+        except:
+            print("Model not found. Trying to train model.")
+            if self.auto_train():
+                print("Model trained and loaded successfully.")
+                return True
+            else:
+                print("Unable to train model!")
+                return False
 
-test = faceTrain()
-test.train()
